5dec70dc34dec31ed40a49d4fe09d708699095d3
Dan <djclissold@yahoo.co.uk>
2016-07-02 16:20:13 +0100

    Finished Fuzzbuzz program and tested up to the number 200

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,12 @@
 def fizzbuzz(number)
-  'fizz'
+  if number %3 == 0 && number %5 == 0
+    'Fizzbuzz'
+  elsif number %3 == 0
+    'Fizz'
+  elsif number %5 == 0
+    'Buzz'
+  else
+    number
+  end
 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,23 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  (1...200).each do |n|
+    if n % 3 == 0 && n % 5 == 0
+  it 'returns "Fizzbuzz" when passed a number divisable by both 3 & 5' do
+    expect(fizzbuzz(n)).to eq 'Fizzbuzz'
+    end
+  elsif n % 3 == 0
+    it 'returns "Fizz" when passed a number divisable by 3' do
+      expect(fizzbuzz(n)).to eq 'Fizz'
+    end
+  elsif n % 5 == 0
+    it 'returns "Buzz" when passed a number divisable by 5' do
+      expect(fizzbuzz(n)).to eq 'Buzz'
+    end
+  else
+    it 'returns the number given when passed a number that is divisable by neither 3 or 5' do
+      expect(fizzbuzz(n)).to eq n
+    end
   end
+end
 end
